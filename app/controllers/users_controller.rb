019f57b23997159ca46823b73f87ad8a7f9e8c96
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-19 08:30:55 -0400

    added controller actions in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user, status: :ok
+        else
+            not_found_response
+        end
+    end
+
+    private
+
+    def not_found_response
+        render json: {error:"User not found"}, status: :not_found
+    end
 end
