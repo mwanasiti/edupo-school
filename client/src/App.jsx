fce749141d1ed89b226a647346696a69cc6c83ec
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-21 20:50:31 +0300

    use useeffect to fetch addmin auth from backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,15 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Login from './components/login';
 function App() {
+
+  useEffect(() => {
+    fetch("/admin_auth").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
   return (
     <div className="App">
       <Login />
