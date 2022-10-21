ee6d7a370a01f99d60c223652758ba9cb3505f42
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-21 23:32:49 +0300

    milestone:added 3 validations

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -3,5 +3,9 @@ class Teacher < ApplicationRecord
 
     has_many :students
    
+    validates :full_name, presence: true, uniqueness: true
+    validates :full_name, length: { minimum: 3 }
     
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid' }
+
 end
