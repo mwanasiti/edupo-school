59802cc2af7fd2a59e1a21735e6f853794e9c69a
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-21 22:41:34 +0300

    milestone:added the has_many macro to the teachers.rb

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,12 +1,7 @@
 class Teacher < ApplicationRecord
     has_secure_password
 
-
-    belongs_to :parent
-    has_many :students, through: :allocations
-    has_many :subjects , through: :allocations
-    has_many :classes
-    has_many :assignments
-    has_many :assessments
+    has_many :students
+   
     
 end
