91a321e60a66dc6b7653742546ce4032a8312ef1
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-21 23:31:38 +0300

    milestone:added 3 validations

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,4 +1,11 @@
 class Parent < ApplicationRecord
     has_secure_password
     has_many :students
+
+
+    validates :full_name, presence: true, uniqueness: true
+    validates :full_name, length: { minimum: 3 }
+    
+    validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid' }
+
 end
