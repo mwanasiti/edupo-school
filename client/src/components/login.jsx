2f6854c4e9fe4dc41de81c5e9db63ba27a7f8972
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-21 23:50:38 +0300

    add handleadminsubmit

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+// import React from 'react'
 import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
+
+function handleAdminSubmit(e) {
+    e.preventDefault();
+    fetch("/admin_login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => {
+          setUser(user);
+        });
+        navigate("/admin");
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
 
 
 const Login=()=>{
@@ -18,14 +43,14 @@ const Login=()=>{
                     <h2>Sign In</h2>
                 </Grid>
                 <TextField label='Username' placeholder='Enter usernmame' fullWidth required/>
-                <input
+                {/* <input
           required
           className=" mt-2 h-8 rounded-lg"
           type="text"
           name="username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-        />
+        /> */}
                 <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
                
                 <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
