ea54eebe4d29b335a35e8b60d3668dbea121061f
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-10-21 22:42:25 +0300

    milestone:added the macros to the student.rb file

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,7 +5,9 @@ class Student < ApplicationRecord
     belongs_to :parent
     belongs_to :subject
     belongs_to :classroom
-    has_many :student_assignments t
-    has_many :assessments
+    has_many :student_assignments
+    has_many :assignments, through: :student_assignments
+    has_many :student_assessments
+    has_many :assessments, through: :student_assessments
    
 end
