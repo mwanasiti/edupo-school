eebbcf50c4d2764cf420e3913aae14195e65023e
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-21 15:35:03 -0400

    milestone:edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :patch, :create]
-  resources :admins, only: [:index, :show, :patch, :create]
-  resources :assignments, only: [:index, :show, :patch, :create]
-  resources :assessments, only: [:index, :show, :patch, :create]
-  resources :classrooms, only: [:index, :show, :patch, :create]
-  resources :subjects, only: [:index, :show, :patch, :create]
-  resources :teachers, only: [:index, :show, :patch, :create]
-  resources :parents, only: [:index, :show, :patch, :create]
-  resources :students, only: [:index, :show, :patch, :create]
+  resources :users, only: [:index, :show, :update, :create]
+  resources :admins, only: [:index, :show, :update, :create]
+  resources :assignments, only: [:index, :show, :update, :create]
+  resources :assessments, only: [:index, :show, :update, :create]
+  resources :classrooms, only: [:index, :show, :update, :create]
+  resources :subjects, only: [:index, :show, :update, :create]
+  resources :teachers, only: [:index, :show, :update, :create]
+  resources :parents, only: [:index, :show, :update, :create]
+  resources :students, only: [:index, :show, :update, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
