f2c0ee9570a4f109bd71e6bf0de5fe7d8c3ce2cc
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-23 17:22:31 +0300

    update loginform

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import "./LoginForm.css";
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 // import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+
+const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
+const avatarStyle={backgroundColor:'#1bbd7e'}
+const btnstyle={margin:'8px 0'}
 
 function LoginForm({ setUser }) {
   const navigate = useNavigate();
@@ -109,6 +116,41 @@ function LoginForm({ setUser }) {
         <label htmlFor="username" className="text-l">
           UserName:
         </label>
+
+        {/* <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required/>
+                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
+                <FormControlLabel
+                    control={
+                    <Checkbox
+                        name="checkedB"
+                        color="primary"
+                    />
+                    }
+                    label="Remember me"
+                 />
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                <Typography >
+                     <Link href="#" >
+                        Forgot password ?
+                </Link>
+                </Typography>
+                <Typography > Do you have an account ?
+                     <Link href="#" >
+                        Sign Up 
+                </Link>
+                </Typography>
+            </Paper>
+        </Grid>
+         */}
+
+
+      
         <input
           required
           className=" mt-2 h-8 rounded-lg"
