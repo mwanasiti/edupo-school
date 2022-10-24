e041dcc69a9aa878b7880ec3781965e3531422ae
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-24 15:02:23 -0400

    creating header component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -52,14 +52,14 @@ function Navbar({ user, setUser }) {
 
   return (
     <nav className="flex items-center justify-between flex-wrap bg-our-black p-6">
-      <Link to="/" className="font-black text-2xl">
+      <Link to="/" className="heading" style={{fontSize:'35px',fontWeight:'bolder', fontFamily:'cursive',color:'black'}}>
         EDUPO
       </Link>
 
       {user ? (
         <button onClick={handleLogout}>Logout</button>
       ) : (
-        <Link to="/login">Login</Link>
+        <Link style={{fontSize:'28px', fontWeight:'bold', fontFamily:'cursive'}} to="/login">Login</Link>
       )}
     </nav>
   );
@@ -67,56 +67,4 @@ function Navbar({ user, setUser }) {
 
 export default Navbar;
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  
-  <div class="container">
-   
-    <a class="navbar-brand me-2" href="https://mdbgo.com/">
-      <img
-        src="https://mdbcdn.b-cdn.net/img/logo/mdb-transaprent-noshadows.webp"
-        height="16"
-        alt="MDB Logo"
-        loading="lazy"
-        style="margin-top: -1px;"
-      />
-    </a>
 
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-mdb-toggle="collapse"
-      data-mdb-target="#navbarButtonsExample"
-      aria-controls="navbarButtonsExample"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <i class="fas fa-bars"></i>
-    </button>
-
-    
-    <div class="collapse navbar-collapse" id="navbarButtonsExample">
-   
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link" href="#">Dashboard</a>
-        </li>
-      </ul>
-     
-
-      <div class="d-flex align-items-center">
-        <button type="button" class="btn btn-link px-3 me-2">
-          Login
-        </button>
-        
-        <a
-          class="btn btn-dark px-3"
-          href="https://github.com/mdbootstrap/mdb-ui-kit"
-          role="button"
-          ><i class="fab fa-github"></i
-        ></a>
-      </div>
-    </div>
-    
-  </div>
-
-</nav>
