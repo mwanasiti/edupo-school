cd317fe26c46c38bca65bb8e97fe6f9033f3c3d5
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-24 14:15:02 -0400

    creating home component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>This will be the landing Page</div>
+    <div className='container'>
+      <div className='row'>
+     
+          <div
+            className="bg-image hover-overlay ripple"
+            style={{backgroundImage: "url('https://www.economist.com/sites/default/files/images/2019/01/articles/main/20190105_usp502.jpg')", 
+            height: '100vh'}}>
+          <div className="mask" style={{backgroundColor: "rgba(0, 0, 0, 0.6)"}}>
+              <div className="d-flex justify-content-center align-items-center h-100">
+                <h1 className="text-white mb-0" style={{fontWeight:'bold', fontFamily:'cursive', fontSize:'70px'}}>OUR MANAGEMENT SYSTEM</h1>
+              </div>
+            </div>
+          </div>
+
+      </div>
+
+    </div>
   )
 }
 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -51,7 +51,7 @@ function Navbar({ user, setUser }) {
   }
 
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-our-blue p-6">
+    <nav className="flex items-center justify-between flex-wrap bg-our-black p-6">
       <Link to="/" className="font-black text-2xl">
         EDUPO
       </Link>
