f00e0ca98c389577c6fecd9bb7160035ac72800a
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-24 11:03:01 +0300

    Milestone: Subject controller CRUD edited

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -11,38 +11,38 @@ class SubjectsController < ApplicationController
       render json: subject
     end
 
- #  CREATE
- def create
-  subject = Subject.create(subject_params)
-  render json: subject,  status: :created
-end
-# UPDATE
-def update
-  subject =Subject.find_by(id: params[:id])
+#  #  CREATE
+#  def create
+#   subject = Subject.create(subject_params)
+#   render json: subject,  status: :created
+# end
+# # UPDATE
+# def update
+#   subject =Subject.find_by(id: params[:id])
   
-  if subject
-    subject.update(subject_params
-    )
-    render json: subject        
-  else
-    render json: {error: "Subject not found"}, status: :not_found
-  end
-end
-# DELELET
-def destroy
-subject =Subject.find_by(id: params[:id])
+#   if subject
+#     subject.update(subject_params
+#     )
+#     render json: subject        
+#   else
+#     render json: {error: "Subject not found"}, status: :not_found
+#   end
+# end
+# # DELELET
+# def destroy
+# subject =Subject.find_by(id: params[:id])
 
-if subject 
-subject.destroy
-head :no_content 
-else
-render json: {error: "Subject not found"}, status: :not_found
-end
+# if subject 
+# subject.destroy
+# head :no_content 
+# else
+# render json: {error: "Subject not found"}, status: :not_found
+# end
 
-end
+# end
 
-private
-def subject_params
-  params.permit(:name)
-end
+# private
+# def subject_params
+#   params.permit(:name)
+# end
 end
