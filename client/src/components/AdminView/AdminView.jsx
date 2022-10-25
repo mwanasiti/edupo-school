c379c3b8b0c1df46ec27e2e443948d8669ac2669
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-25 19:11:29 +0300

    Milestone: Styling of containers

--- client/src/components/AdminView/AdminView.jsx
+++ client/src/components/AdminView/AdminView.jsx
@@ -1,20 +1,21 @@
 import { Grid } from '@material-ui/core'
 import React from 'react'
 import Adminbar from './Forms/Adminbar'
-import StudentForm from './Forms/StudentForm'
+import StudentForm from './Forms/Student/StudentForm'
+
 
 
 function AdminView() {
   return (
     <div>
-     <p> This will be the Admin's view after Logging in <br/></p>
+     {/* <p> This will be the Admin's view after Logging in <br/></p> */}
 
       <Grid container>
-      <Grid item sm={4}>
+      <Grid item sm={4} xs={2} lg={2}>
       <Adminbar/>
       </Grid>
-      <Grid item sm={7}>
-      <StudentForm/>
+      <Grid item sm={7} xs={10} lg={5}>
+      <StudentForm />
       </Grid>
       </Grid>
    
