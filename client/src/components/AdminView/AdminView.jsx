a6d58feec6b0e6ae6648a9bc331c13ad43be0fd3
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-25 14:11:54 +0300

    Milestone: Body and sidebar rendered on adminview

--- client/src/components/AdminView/AdminView.jsx
+++ client/src/components/AdminView/AdminView.jsx
@@ -1,8 +1,27 @@
+import { Grid } from '@material-ui/core'
 import React from 'react'
+import Adminbar from './Forms/Adminbar'
+import StudentForm from './Forms/StudentForm'
+
 
 function AdminView() {
   return (
-    <div>This will be the Admin's view after Logging in</div>
+    <div>
+     <p> This will be the Admin's view after Logging in <br/></p>
+
+      <Grid container>
+      <Grid item sm={4}>
+      <Adminbar/>
+      </Grid>
+      <Grid item sm={7}>
+      <StudentForm/>
+      </Grid>
+      </Grid>
+   
+    
+    {/* <StudentForm/>
+    <StudentAdd/> */}
+    </div>
   )
 }
 
