d76160576e5571575bb436f362472fcf09a2581b
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-10-25 16:04:32 +0300

    Added student add form

--- client/src/components/AdminView/AdminView.jsx
+++ client/src/components/AdminView/AdminView.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import StudentAdd from './StudentAdd';
+
 
 function AdminView() {
   return (
-    <div>This will be the Admin's view after Logging in</div>
+    <>
+      <div>This will be the Admin's view after Logging in</div>
+      <StudentAdd />
+    </>
+
   )
 }
 
