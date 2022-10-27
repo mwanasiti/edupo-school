a3807c31fb6c8f8dc5baf976272cfb7fc4b94a9d
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-27 22:32:47 +0300

    Milestone: Parent form created

--- client/src/components/AdminView/Forms/Parent/ParentForm.jsx
+++ client/src/components/AdminView/Forms/Parent/ParentForm.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import "./Parent.css";
 
 function ParentForm() {
   return (
-    <div>ParentForm</div>
-  )
+    <div className="new">
+      <div className="top">
+        <h1>Add Parent</h1>
+      </div>
+      <div className="bottom">
+        <div className="left">left
+        <img src="https://i.pinimg.com/564x/a9/cc/d7/a9ccd7f3b82fc8b70c6d75a299302001.jpg" alt='profile'/>
+        </div>
+        <div className="right"> 
+        <form>
+        <div className="formInput">
+          <label></label>
+        </div>
+        </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ParentForm
+export default ParentForm;
