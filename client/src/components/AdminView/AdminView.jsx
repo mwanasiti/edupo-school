2fb89eb2df8be17ef3238bee468248113362a0d7
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-27 21:03:44 +0300

    Milestone: Parent Table rendered

--- client/src/components/AdminView/AdminView.jsx
+++ client/src/components/AdminView/AdminView.jsx
@@ -1,21 +1,30 @@
 import { Grid } from '@material-ui/core'
 import React from 'react'
+import Dashboard from './Dashboard/Dashboard'
+import ParentView from './DataTables/ParentView'
+
 import Adminbar from './Forms/Adminbar'
-import StudentForm from './Forms/Student/StudentForm'
+
+
 
 
 
 function AdminView() {
   return (
     <div>
+  
      {/* <p> This will be the Admin's view after Logging in <br/></p> */}
 
       <Grid container>
       <Grid item sm={4} xs={2} lg={2}>
       <Adminbar/>
       </Grid>
-      <Grid item sm={7} xs={10} lg={5}>
-      <StudentForm />
+     
+      <Grid item sm={7} xs={10} lg={9}>
+      {/* <StudentData /> */}
+      {/* <Dashboard/> */}
+      {/* <TeacherView/> */}
+      <ParentView/>
       </Grid>
       </Grid>
    
