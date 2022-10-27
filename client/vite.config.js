0a0f0f0c86299988618cdad512bd127dd5164eb8
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-27 03:35:39 -0400

    student parent proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -71,7 +71,7 @@ export default defineConfig({
         secure: false,
       },
       "/students": {
-        target: "http://localhost:3000",
+        target: "http://localhost:3000"
         changeOrigin: true,
         secure: false,
       },
