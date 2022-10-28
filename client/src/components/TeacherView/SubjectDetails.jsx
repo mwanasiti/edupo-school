0a394bb18e7792b4ae12e6f038f95cf50ed5e358
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-28 19:59:18 +0300

    add subject details

---
+++ client/src/components/TeacherView/SubjectDetails.jsx
@@ -0,0 +1,33 @@
+import { useParams, NavLink, useNavigate } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import { useContext } from "react";
+// import { UserContext } from "../context/user";
+// import SyllabusForm from './SyllabusForm';
+// import DeleteConfirmation from './DeleteConfirmation';
+function SubjectDetails() {
+    return (
+      <div>
+        <h1>Begin building your subject by adding a description for your class</h1>
+    
+        
+      </div>
+    )
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default SubjectDetails
--- client/src/components/TeacherView/TeacherView.jsx
+++ client/src/components/TeacherView/TeacherView.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import Assignment from './Assignment.jsx'
+// import Assignment from './Assignment.jsx'
+import SubjectDetails from '../../components/TeacherView/SubjectDetails'
 
 
 function TeacherView() {
   return (
     <div>
       This will be the Teacher's view after Logging in
-      <Assignment /> 
+      {/* <Assignment />  */}
+      <SubjectDetails />
     </div>
   )
 }
