1ca4ec29441c56e813dcede4bb844be6776c17b7
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-10-28 14:57:16 +0300

    Add get and show action for student_assesments controller

--- app/controllers/student_assesments_controller.rb
+++ app/controllers/student_assesments_controller.rb
@@ -1,2 +1,18 @@
 class StudentAssesmentsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+
+
+    def index
+        render json: StudentAssesment.all
+    end
+
+    def show
+        student_assessment = StudentAssesment.find(params[:id])
+        render json: student_assessment
+    end
+
+    def render_record_not_found
+        render json: {error: "Student Assessment not found"}, status: 404
+    end
+
 end
