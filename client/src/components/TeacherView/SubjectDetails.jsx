b147ef7723cc1110d8727dab1248e096ce0170f0
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-29 15:04:15 +0300

    import context

--- client/src/components/TeacherView/SubjectDetails.jsx
+++ client/src/components/TeacherView/SubjectDetails.jsx
@@ -1,7 +1,7 @@
 import { useParams, NavLink, useNavigate } from 'react-router-dom'
 import { useState, useEffect } from "react";
 import { useContext } from "react";
-// import { UserContext } from "../context/user";
+import { UserContext } from "../../context/user";
 // import SyllabusForm from './SyllabusForm';
 import DeleteConfirmation from '../../components/TeacherView/DeleteConfirmation';
 function SubjectDetails({  }) {
