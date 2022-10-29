fa5d7733b78783479bc8fac80b372adf98f22179
AWITI0301 <johnoketchawiti@gmail.com>
2022-10-29 16:29:44 +0300

    import the teacher pages

--- client/src/components/TeacherView/TeacherView.jsx
+++ client/src/components/TeacherView/TeacherView.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 // import Assignment from './Assignment.jsx'
 import SubjectDetails from '../../components/TeacherView/SubjectDetails'
+import Syllabus from './Syllabus'
 
 
 function TeacherView() {
   return (
     <div>
-      This will be the Teacher's view after Logging in
+      {/* This will be the Teacher's view after Logging in */}
       {/* <Assignment />  */}
       <SubjectDetails />
+      < Syllabus/>
     </div>
   )
 }
