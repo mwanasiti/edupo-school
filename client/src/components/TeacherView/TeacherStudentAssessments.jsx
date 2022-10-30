9e63f6484ad40a0da3491e31717f43169db13732
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-10-30 15:34:35 +0300

    Update progress Tables to include subject

--- client/src/components/TeacherView/TeacherStudentAssessments.jsx
+++ client/src/components/TeacherView/TeacherStudentAssessments.jsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
-
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -13,51 +12,59 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 
-
 function TeacherStudentAssessments() {
   const navigate = useNavigate();
   const params = useParams();
-  const {id} = params
-  const [assesments, setAssesments] = useState([])
+  const { id } = params;
+  const [assesments, setAssesments] = useState([]);
 
   useEffect(() => {
-      fetch(`/par_stu_assesments/${id}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setAssesments(data);
-        });
-    }, []);
+    fetch(`/par_stu_assesments/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setAssesments(data);
+      });
+  }, []);
 
   return (
     <>
       {/* <div>TeacherStudentAssessments</div> */}
       <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Student Name</TableCell>
-            <TableCell align="right">Assessment</TableCell>
-            <TableCell align="right">Out Of</TableCell>
-            <TableCell align="right">Score</TableCell>
-            <TableCell align="right">Status</TableCell>
-            <TableCell align="right">Change Score</TableCell>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Student Name</TableCell>
+              <TableCell align="right">Assessment</TableCell>
+              <TableCell align="right">Subject</TableCell>
 
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {assesments.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.student}
-              </TableCell>
-              <TableCell align="right">{row.assessment}</TableCell>
-              <TableCell align="right">{row.total}</TableCell>
-              <TableCell align="right">{row.score ? row.score : 0}</TableCell>
-              <TableCell align="right">{row.score >= 40 ? <h1 className="text-green-600">PASS</h1> : <h1 className="text-red-500">FAIL</h1>}</TableCell>
-              <TableCell align="right">
+              <TableCell align="right">Out Of</TableCell>
+              <TableCell align="right">Score</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right">Change Score</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {assesments.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.student}
+                </TableCell>
+                <TableCell align="right">{row.assessment}</TableCell>
+                <TableCell align="right">{row.subject}</TableCell>
+
+                <TableCell align="right">{row.total}</TableCell>
+                <TableCell align="right">{row.score ? row.score : 0}</TableCell>
+                <TableCell align="right">
+                  {row.score >= 40 ? (
+                    <h1 className="text-green-600">PASS</h1>
+                  ) : (
+                    <h1 className="text-red-500">FAIL</h1>
+                  )}
+                </TableCell>
+                <TableCell align="right">
                   <Button
                     variant="contained"
                     color="success"
@@ -68,11 +75,11 @@ function TeacherStudentAssessments() {
                     Change Score
                   </Button>
                 </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
