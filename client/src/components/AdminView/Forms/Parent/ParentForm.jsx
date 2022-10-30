2ac6d40175a5d5574d35c56bb1cd3acebf407f88
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-30 23:24:03 +0300

    Milestone: Parent Form labels added and styled

--- client/src/components/AdminView/Forms/Parent/ParentForm.jsx
+++ client/src/components/AdminView/Forms/Parent/ParentForm.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@material-ui/core";
 import React from "react";
 import "./Parent.css";
 
@@ -8,24 +9,46 @@ function ParentForm() {
         <h1>Add Parent</h1>
       </div>
       <div className="bottom">
-        <div className="left">left
-        <img src="https://i.pinimg.com/564x/a9/cc/d7/a9ccd7f3b82fc8b70c6d75a299302001.jpg" alt='profile'/>
+        <div className="left">
+          left
+          <img
+            src="https://i.pinimg.com/564x/a9/cc/d7/a9ccd7f3b82fc8b70c6d75a299302001.jpg"
+            alt="profile"
+          />
         </div>
-        <div className="right"> 
-        <form>
-        <div className="formInput">
-          <label>Gender</label>
-        </div>
-        <div className="formInput">
-          <label>Gender</label>
-        </div>
-        <div className="formInput">
-          <label>Gender</label>
-        </div>
-        <div className="formInput">
-          <label>Gender</label>
-        </div>
-        </form>
+        <div className="right">
+          <form>
+            <div className="formInput">
+              <label>Address:</label>
+              <input type="text" placeholder="Address" />
+            </div>
+            <div className="formInput">
+              <label>Phone Number:</label>
+              <input type="text" placeholder="Phone_number" />
+            </div>
+            <div className="formInput">
+              <label>Full Name:</label>
+              <input type="text" placeholder="Full Name" />
+            </div>
+            <div className="formInput">
+              <label>Email:</label>
+              <input type="text" placeholder="Email" />
+            </div>
+            <div className="formInput">
+              <label>Password:</label>
+              <input type="password"  />
+            </div>
+            <div className="formInput">
+            <Button variant="contained" color="primary" style={{
+                width: "450px",
+                // marginTop: "15px",
+                marginLeft: "55px",
+                alignItems: "center",
+              }}
+              type="submit">Send</Button>
+            </div>
+            
+          </form>
         </div>
       </div>
     </div>
