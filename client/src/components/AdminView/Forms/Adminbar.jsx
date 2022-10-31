bc86ad156002ab3331629f3f4b7d38dd0bec9697
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-31 11:38:55 +0300

    Milestone: Click events added

--- client/src/components/AdminView/Forms/Adminbar.jsx
+++ client/src/components/AdminView/Forms/Adminbar.jsx
@@ -1,69 +1,93 @@
-import React from 'react'
-import {Button, Container, makeStyles}  from "@material-ui/core"
-import { BorderColor, Dashboard, Group, Home, Person, School, Settings } from '@material-ui/icons';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Button, Container, makeStyles } from "@material-ui/core";
+import {
+  BorderColor,
+  Dashboard,
+  Group,
+  Home,
+  Person,
+  School,
+  Settings,
+} from "@material-ui/icons";
+import { useNavigate } from "react-router-dom";
 
-const useStyles = makeStyles(theme=>({
-  container:{
+const useStyles = makeStyles((theme) => ({
+  container: {
     paddingTop: theme.spacing(3),
     backgroundColor: "#E5E4E2",
-    height: "150vh"
+    height: "150vh",
   },
-icon:{
-  marginRight: theme.spacing(1),
-  [theme.breakpoints.up("sm")]:{
-    fontSize: "18px",
-  }
-
-},
-  item:{
+  icon: {
+    marginRight: theme.spacing(1),
+    [theme.breakpoints.up("sm")]: {
+      fontSize: "18px",
+    },
+  },
+  item: {
     display: "flex",
     alignItems: "center",
     marginBottom: theme.spacing(4),
-    [theme.breakpoints.up("sm")]:{
-      marginBottom:theme.spacing(3),
-      cursor: "pointer"
-    }
-  }
-
+    [theme.breakpoints.up("sm")]: {
+      marginBottom: theme.spacing(3),
+      cursor: "pointer",
+    },
+  },
 }));
 
 function Adminbar() {
   let navigate = useNavigate();
-    const classes = useStyles();
+  const classes = useStyles();
   return (
-   <Container className={classes.container}>
-    
-    {/* <div className={classes.item}>
+    <Container className={classes.container}>
+      {/* <div className={classes.item}>
     <Home className={classes.icon}/> Home<br/>
     </div> */}
-    <div  className={classes.item}> 
-      <Dashboard  className={classes.icon} onClick={()=>{ navigate('/dashboard')}}/> Dashboard
-      {/* <Typography className={classes.text}>Dashboard</Typography> */}
-    </div>
-    <div  className={classes.item}>
-    
-    <School className={classes.icon} /> Student
-    </div>
-    <div  className={classes.item}>
-    <BorderColor className={classes.icon}onClick={()=>{ navigate('/teacherform')}} /> Teacher
-    </div>
-    <div  className={classes.item}>
-    <Person className={classes.icon}/> Parent
-    </div>
-    <div  className={classes.item}>
-    <Group className={classes.icon}/> Staff
-    </div>
-    <div  className={classes.item}>
-    <Settings className={classes.icon}/>  Settings
-    </div>
-    {/* <div>
+      <div
+        className={classes.item}
+        onClick={() => {
+          navigate("/dashboard");
+        }}
+      >
+        <Dashboard className={classes.icon} /> Dashboard
+        {/* <Typography className={classes.text}>Dashboard</Typography> */}
+      </div>
+      <div className={classes.item} 
+      onClick={() => {
+          navigate("/addstudent");
+        }}
+      >
+        <School className={classes.icon} /> Student
+      </div>
+      <div
+        className={classes.item}
+        onClick={() => {
+          navigate("/addteacher");
+        }}
+      >
+        <BorderColor className={classes.icon} /> Teacher
+      </div>
+      <div className={classes.item} 
+      onClick={() => {
+          navigate("/addparent");
+        }}
+      >
+        <Person className={classes.icon} /> Parent
+      </div>
+      <div className={classes.item}
+      onClick={() => {
+          navigate("/addstaff");
+        }}
+      >
+        <Group className={classes.icon} /> Staff
+      </div>
+      <div className={classes.item}>
+        <Settings className={classes.icon} /> Settings
+      </div>
+      {/* <div>
     <Button variant="outlined" color="secondary" size="large" onClick={()=>{ navigate('/studentform')}}>StudentForm</Button>
     </div> */}
-    
-    
-   </Container>
-  )
+    </Container>
+  );
 }
 
-export default Adminbar
+export default Adminbar;
