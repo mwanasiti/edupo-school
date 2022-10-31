d0c0ca473d3621a34845d65c497f7991bdfed31b
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-31 14:43:52 +0300

    Milestone: Sidebar rendered on student section

--- client/src/components/AdminView/Forms/Student/StudentForm.jsx
+++ client/src/components/AdminView/Forms/Student/StudentForm.jsx
@@ -1,12 +1,19 @@
-import { Container } from '@material-ui/core'
-import React from 'react'
+import { Container, Grid } from "@material-ui/core";
+import React from "react";
 
 function StudentForm() {
   return (
-   <Container>
-    <h1>Forms</h1>
-   </Container>
-  )
+    <div className="bigParent">
+      <Grid container>
+        <Grid item sm={4} xs={2} lg={2}>
+          <Sidebar />
+        </Grid>
+        <Grid item sm={7} xs={10} lg={9}>
+          <h1>Student Form</h1>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default StudentForm
+export default StudentForm;
