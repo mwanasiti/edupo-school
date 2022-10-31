e5ad89548fd9166747e0e10568f5d08e65ca0b88
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2022-11-01 00:07:08 +0300

    teacher able to add new subject

--- app/controllers/subject_teachers_controller.rb
+++ app/controllers/subject_teachers_controller.rb
@@ -1,4 +1,6 @@
 class SubjectTeachersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_record_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unproccessable_entity
 
     def teacher_subjects
         @teacher = Teacher.find_by(id: session[:teacher_id])
@@ -8,6 +10,26 @@ class SubjectTeachersController < ApplicationController
         else
         render json: {errors: ["Please Log in as Teacher to view your subjects"]}, status: 401  
         end
+    end
+
+    def new_teacher_subject
+        subject = Subject.create(name: params[:name])
+        subject_teacher = SubjectTeacher.create(subject_id: subject.id, teacher_id: session[:teacher_id])
+        render json: subject_teacher, status: 201
+    end
+
+    private
+
+    def subject_teacher_params
+        params.permit(:name, :subject_id, :teacher_id)
+    end
+
+    def render_record_not_found
+        render json: {errors: "Student Assignment not found"}, status: 404
+    end
+
+    def render_unproccessable_entity(invalid)
+        render  json: {errors: invalid.record.errors.full_messages}, status: 422
     end
     
 end
