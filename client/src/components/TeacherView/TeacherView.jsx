de5c2688c3e29f2e13cdcf21508a1165c47e1b9e
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-10-31 07:49:55 -0400

    adding editorcontainer to teacherview

--- client/src/components/TeacherView/TeacherView.jsx
+++ client/src/components/TeacherView/TeacherView.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import Assignment from './Assignment.jsx'
-import AssignmentList from '../TeacherView/AssignmentList'
+// import AssignmentList from '../TeacherView/AssignmentList'
+import EditorContainer from './EditorContainer.jsx'
 
 function TeacherView() {
   return (
+    <>
     <div>
-      This will be the Teacher's view after Logging in
-      
-      {/* <Assignment /> */} 
-    </div>
+      This will be the Teacher's view after Logging in...
+      <EditorContainer/>
+      <Assignment />
+      </div>
+    </>
   )
 }
 
