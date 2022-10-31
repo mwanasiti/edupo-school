ca938c06ebd11f769f1230b3406a5018cff227c2
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-31 14:39:23 +0300

    Milestone: Dependency modifications

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import AdminView from "./components/AdminView/AdminView";
+import Sidebar from "./components/AdminView/BarRoutes/Sidebar";
+import Dashboard from "./components/AdminView/Dashboard/Dashboard";
+import ParentForm from "./components/AdminView/Forms/Parent/ParentForm";
+import StudentForm from "./components/AdminView/Forms/Student/StudentForm";
+import TeacherForm from "./components/AdminView/Forms/Teacher/TeacherForm";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 import LoginForm from "./components/LoginForm/LoginForm";
@@ -8,6 +13,10 @@ import Navbar from "./components/Navbar/Navbar";
 import ParentView from "./components/ParentView/ParentView";
 import StudentView from "./components/StudentView/StudentView";
 import TeacherView from "./components/TeacherView/TeacherView";
+
+
+
+
 
 function App() {
   const [user, setUser] = useState(null);
@@ -47,7 +56,10 @@ function App() {
   return (
     <>
       <main className="min-h-[92vh]">
-        <Navbar user={user} setUser={setUser} />
+      <Navbar user={user} setUser={setUser} />
+    
+      {/* <Sidebar /> */}
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/teacher" element={<TeacherView />} />
@@ -55,6 +67,13 @@ function App() {
           <Route path="/parent" element={<ParentView />} />
           <Route path="/admin" element={<AdminView />} />
           <Route path="/login" element={<LoginForm setUser={setUser} />} />
+
+          {/* ADMIN ROUTES */}
+          
+        <Route  path='/dashboard' element={<Dashboard/>}/>
+        <Route  path='/addstudent' element={<StudentForm/>}/>
+        <Route  path='/addteacher' element={<TeacherForm/>}/>
+        <Route  path='/addparent' element={<ParentForm/>}/>
         </Routes>
       </main>
       <Footer />
