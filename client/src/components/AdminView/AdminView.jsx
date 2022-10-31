f29f3a1b538bfa24b4958560132e19582fc6ad5c
Olivia Adongo <akinyioliviatove@gmail.com>
2022-10-31 13:03:12 +0300

    Milestone: Rendering component

--- client/src/components/AdminView/AdminView.jsx
+++ client/src/components/AdminView/AdminView.jsx
@@ -1,7 +1,7 @@
 import { Grid } from '@material-ui/core'
 import React from 'react'
 import Sidebar from './BarRoutes/Sidebar'
-import Widgets from './Cardboard/Widgets'
+
 import Dashboard from './Dashboard/Dashboard'
 import ParentView from './DataTables/ParentView'
 import StudentData from './DataTables/StudentData'
@@ -13,7 +13,7 @@ import StudentForm from './Forms/Student/StudentForm'
 import TeacherForm from './Forms/Teacher/TeacherForm'
 
 
-
+ 
 
 
 function AdminView() {
@@ -24,14 +24,13 @@ function AdminView() {
 
       <Grid container>
       <Grid item sm={4} xs={2} lg={2}>
-      <Adminbar/>
-      {/* <Sidebar/> */}
+      {/* <Adminbar/> */}
+      <Sidebar/>
       </Grid>
-     
       <Grid item sm={7} xs={10} lg={9}>
-      {/* <Widgets/> */}
+      
       {/* <StudentForm/> */}
-      <ParentForm/>
+      {/* <ParentForm/> */}
       {/* <StudentData /> */}
       {/* <Dashboard /> */}
       {/* <TeacherView/> */}
