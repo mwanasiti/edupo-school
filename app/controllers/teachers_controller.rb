1668ccc0ef913204afb6ef4e6027662be62e90ee
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-11-01 14:28:35 +0300

    milestone:adding incomming changes

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,9 +1,5 @@
 class TeachersController < ApplicationController
   # before_action :set_teacher, only: [:show, :edit, :update, :destroy]
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/angela
     def index
         teachers = Teacher.all
         render json: teachers
@@ -14,18 +10,10 @@ class TeachersController < ApplicationController
         if teacher
             render json: teacher
         else
-<<<<<<< HEAD
-
-            render json: {error: "Teacher not found"}, status: :not_found
-        end
-    end
-
-=======
            # render json: {error: “Teacher not found”}, status: :not_found
            render json: {error: "Teacher not found"}, status: :not_found
         end
     end
->>>>>>> origin/angela
   #  CREATE
     def create
       teacher = Teacher.create(teacher_params)
@@ -39,20 +27,12 @@ class TeachersController < ApplicationController
 # UPDATE
     def update
       teacher =Teacher.find_by(id: params[:id])
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/angela
       
       if teacher
         teacher.update(teacher_params)
         render json: teacher        
       else
         render json: {error: "Teacher not found"}, status: :not_found
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/angela
       end
     end
 # DELELET
@@ -68,21 +48,11 @@ def destroy
 
 end
    
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/angela
 private
     def teacher_params
       params.permit(:role, :gender, :image,:phone_no, :address, :full_name, :email, :password)
     end
-<<<<<<< HEAD
+
 
 end
-<<<<<<< HEAD
 
-=======
->>>>>>> deno
-=======
-end
->>>>>>> origin/angela
