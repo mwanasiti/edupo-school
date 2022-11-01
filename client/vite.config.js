e5b4ab67c30a5ee67354b0ead353f98f90c65e33
Olivia Adongo <akinyioliviatove@gmail.com>
2022-11-01 16:53:29 +0300

    Milestone: Teacher and Parent proxy added

--- client/vite.config.js
+++ client/vite.config.js
@@ -135,6 +135,16 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+      "/parents": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+      "/teachers": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
