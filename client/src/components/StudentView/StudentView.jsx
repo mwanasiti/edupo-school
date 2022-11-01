c67d78ec8816d0099eef108a162fe08a46eb5ddd
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-11-01 09:36:56 +0300

    milestone:edited the studentview rows

--- client/src/components/StudentView/StudentView.jsx
+++ client/src/components/StudentView/StudentView.jsx
@@ -1,33 +1,35 @@
-import React, {useState, useEffect} from 'react'
+import React from 'react'
 //import axios from 'axios'
 import StudentGrid from './StudentGrid'
 import "./Student.css"
+// import {useParams } from "react-router-dom";
+function StudentView({student}) {
 
-function StudentView() {
-
-  const [students, setStudents] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  // const [student, setStudent] = useState({})
+  // const [isLoading, setIsLoading] = useState(true)
 
 
+  // const params = useParams();
+  //  const { id } = params;
  
   //get request
 
-  useEffect(() => {
-    fetch("http://localhost:3000/students")
-      .then((res) => res.json())
-      .then((data) => {
-        setStudents(data);
-        console.log(data)
-        setIsLoading(false)
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("/student_id")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setStudent(data)
+  //       console.log(data)
+  //       // setIsLoading(false)
+  //     });
+  // }, []);
 
 
   return (
 
     <div className='container'>
-         <p>Welcome Students </p>
-         <StudentGrid isLoading={isLoading} students={students} />
+         <p>Welcome</p>
+         <StudentGrid student={student} />
       </div>
 
 
