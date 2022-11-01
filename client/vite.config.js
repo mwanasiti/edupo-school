7e8a3f61b20f8278b563b7be0861e1af0fdf13ec
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-11-01 13:04:18 +0300

    milestone:adding the proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -145,6 +145,11 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
       },
+      "/studentId": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 });
