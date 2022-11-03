288b323a299c5052b950bcdc241db7088f19ba46
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-11-03 16:17:29 -0400

    milestone:mapping

--- client/src/components/AdminView/DataTables/StudentData.jsx
+++ client/src/components/AdminView/DataTables/StudentData.jsx
@@ -59,7 +59,7 @@ function StudentData() {
         </Grid>
         <Grid item sm={4} xs={2} lg={9}>
        <div className="studentTopic">
-        <p>Student Tabl</p>
+        <p>Student Table</p>
        </div>
           <TableContainer component={Paper} style={{marginLeft:"20px", marginTop:"20px", height:"500px"}}>
           
