f601e8501682df53e5337c2d06cc41bacf12cd39
kanyi-a <angela.kanyi@student.moringaschool.com>
2022-11-03 12:13:23 +0300

    milestone:removing redundant proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -154,11 +154,7 @@ export default defineConfig({
         target: "http://localhost:3000",
         changeOrigin: true,
         secure: false,
-      },
-      "/studentId": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-        secure: false,
+      
       },
     },
   },
