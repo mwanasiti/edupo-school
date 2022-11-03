fa84029bbd0669f151a134a172dd03a505d38dd5
Mariam-Ali <ali.mariam.mwanasiti@gmail.com>
2022-11-03 16:18:09 -0400

    milestone:adding id table cell in teacher table

--- client/src/components/AdminView/DataTables/TeacherData.jsx
+++ client/src/components/AdminView/DataTables/TeacherData.jsx
@@ -55,7 +55,7 @@ function TeacherData() {
         </Grid>
         <Grid item sm={4} xs={2} lg={9}>
         <div className="studentTopic">
-        <p>Teacher Table</p>
+        <p>Teacher Tabl</p>
        </div>
           <TableContainer
             component={Paper}
